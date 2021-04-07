c3769986627f5ba9e9d1d1e1941f196cff6371cf
tod619 <trevorodwyer619@hotmail.com>
2021-04-07 19:41:57 +0100

    add function to generate random password in script.js file

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const lowercaseEl = document.getElementById('lowercase')
+const uppercaseEl = document.getElementById('uppercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const clipboardEl = document.getElementById('clipboard')
+const generateEl = document.getElementById('generate')
+
+
+generateEl.addEventListener('click', () => {
+    const length = +lengthEl.value
+    const hasLower = lowercaseEl.checked
+    const hasUpper = uppercaseEl.checked
+    const hasNumber = numbersEl.checked
+    const hasSymbol = symbolsEl.checked
+
+    resultEl.innerText = generatePassword(hasUpper,hasLower,hasNumber,hasSymbol,length)
+})
+
+function generatePassword(upper,lower,number,symbol,length) {
+    let generatedPassword = ''
+    const typesCount = upper + lower + number + symbol
+    const typesArr = [{upper},{lower},{number},{symbol}].filter(item => Object.values(item)[0])
+    
+    if(typesCount === 0) {
+        return ''
+    }
+
+    for(let i = 0; i < length; i+=typesCount){
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0]
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length)
+    return finalPassword
+}
+
+
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
@@ -24,4 +65,3 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
-console.log(getRandomLower(),getRandomUpper(),getRandomNumber(), getRandomSymbol())
